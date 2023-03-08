c95f0cc160dc4b733794b862e584795cb6a35d00
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-08 14:50:55 +0000

    Create project ManipulaVar. Recieve argument and turn into string. Print argument and a math operation result.

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(args[0]);
+            Console.WriteLine(x);
+
+            int y = 6;
+            int result = x++ * y++;
+            Console.WriteLine(args[0]+"++ times "+y+"++ equals "+result);
+        }
+    }
+}
