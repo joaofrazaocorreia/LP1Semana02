d8c5f135dd3003034a36cdb2530a228a921c3b5b
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 22:31:56 +0000

    Create project Triangle. Print pyramid of given line height and character.

---
+++ Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int line_amount = Convert.ToInt32(args[0]);
+            string character = args[1];
+
+            int line=1;
+            string line_text = "";
+
+            while (line<=line_amount)
+            {        
+                line_text += character;
+
+                Console.WriteLine(line_text);
+                line++;
+            }
+        }
+    }
+}
