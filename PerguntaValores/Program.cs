c537eaba41d6230ae7f0fe768a8698ed821ffea9
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 02:44:40 +0000

    Change variable pi into Math.PI (exercise 5). Change r*r into Math.Pow().

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -14,12 +14,11 @@ namespace PerguntaValores
 
             int a = int.Parse(height);
             int r = int.Parse(radius);
-            double pi = 3.1415926;
 
-            double volume = pi * (r*r) * a;
+            double volume = Math.PI * Math.Pow(r,2) * a;
             Console.WriteLine("The cylinder's volume is "+volume+".");
 
-            double surfc_area = 2 * pi * r * (r+a);
+            double surfc_area = 2 * Math.PI * r * (r+a);
             Console.WriteLine("The cylinder's surface area is "+surfc_area+".");
         }
     }
