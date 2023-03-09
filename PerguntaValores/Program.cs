a8b033b4c392842a2da3877b264a693b0215405e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 02:34:04 +0000

    Add cillinder volume printing.

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -11,6 +11,14 @@ namespace PerguntaValores
 
             Console.WriteLine("Please insert radius:");
             string radius= Console.ReadLine();
+
+            int a = int.Parse(height);
+            int r = int.Parse(radius);
+
+            double volume = Math.PI * (r*r) * a;
+            Console.WriteLine("The cillinder's volume is "+volume+".");
+
+            
         }
     }
 }
