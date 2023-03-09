78f11d493102d396de62119d173de5f3ad9b2143
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 21:45:16 +0000

    Create project ChatGPTMother

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
