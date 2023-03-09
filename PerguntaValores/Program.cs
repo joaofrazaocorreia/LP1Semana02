d2b1643bcd2346f73afdf023372ce578ac1145da
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 02:29:23 +0000

    Create project PerguntaValores. Recieve inputs for height and radius of a cillinder.

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert height:");
+            string height= Console.ReadLine();
+
+            Console.WriteLine("Please insert radius:");
+            string radius= Console.ReadLine();
+        }
+    }
+}
