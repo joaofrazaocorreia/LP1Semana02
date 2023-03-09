6dad030e0edbe11eba10310218d43ba30c26e3c0
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 21:29:53 +0000

    Change switch syntax to use patterns

--- GranpaPatternSwitch/Program.cs
+++ GranpaPatternSwitch/Program.cs
@@ -6,7 +6,25 @@ namespace GranpaPatternSwitch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variáveis auxiliares
+            string question;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            string response = question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            // Responder adequadamente
+            Console.WriteLine(response);
         }
     }
 }
