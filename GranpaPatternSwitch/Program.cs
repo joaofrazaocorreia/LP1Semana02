306b2cf8ee07b82c8c597209ebc97b88b0fc5d7c
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-09 21:14:42 +0000

    Add example projects. Create project GranpaPatternSwitch.

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
