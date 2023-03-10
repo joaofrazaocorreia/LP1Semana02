b9e93f506c52fd3824e194a5796e9a12aec4c1ba
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-10 13:48:17 +0000

    Create project CrossSquare. Print a square of hashtags with a cross of X inside, of the size of a given argument

---
+++ CrossSquare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CrossSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = Convert.ToInt32(args[0]);
+            int line=1;
+            string line_text = "";
+
+            while (line <= amount)
+            {
+                line_text="#";
+
+                if (line==1 || line==amount)
+                {
+                    for (int x=1; x<amount; x++)
+                    {
+                        line_text+= "#";
+                    }
+                }
+
+                else
+                {
+                    for (int x=1; x<=amount; x++)
+                    {
+                        if (x==line-1 || x==amount-line)
+                        {
+                            line_text+="X";
+                        }
+
+                        else if (x==amount-1)
+                        {
+                            line_text+= "#";
+                        }
+
+                        else
+                        {
+                            line_text+= " ";
+                        }
+                            
+                    }
+                }
+                Console.WriteLine(line_text);
+                line++;
+            }
+        }
+    }
+}
